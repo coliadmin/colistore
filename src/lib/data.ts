f22cdf7d97ff72bebb38568633b0ed968feb48aa
coliadmin <thomas.pufahl@colidevs.com>
2025-02-28 19:26:18 -0300

    Update data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,10 +5,10 @@ import {Company} from "@/modules/twenty/company/types";
 import {normalizeUrl, toCompany} from "@/modules/twenty/company/utils";
 
 export async function getData(domain: string): Promise<Company | null> {
-  const url = normalizeUrl(domain, true) + ":3000";
+  const url = normalizeUrl(domain, true);
   const r = url + `/api/company`;
 
-  console.log("lib/domain getDomain | ", r);
+  console.log("lib/domain getData | ", r);
 
   const response = await fetch(r, {next: {tags: [domain]}});
 
